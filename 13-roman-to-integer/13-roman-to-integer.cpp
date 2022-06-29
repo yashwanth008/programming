9840b264decac3a999c0617bb91e94ff8a3f4158
yashwanth008 <76914360+yashwanth008@users.noreply.github.com>
2022-06-29 09:01:38 +0530

    Time: 14 ms (62.41%), Space: 5.9 MB (97.73%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int result = 0;
+        for(int i = 0;i<s.size();i++){
+            switch(s[i]){
+                case 'I':
+                        if(s[i+1]=='V'){
+                            result+=4;
+                            i++;
+                        }
+                        else if(s[i+1]=='X'){
+                            result+=9;
+                            i++;
+                        }
+                        else{
+                            result+=1;
+                            
+                        }
+                    break;
+                case 'V':
+                        result+=5;
+                    break;
+                case 'X':
+                        if(s[i+1]=='L'){
+                            result+=40;
+                            i++;
+                        }
+                        else if(s[i+1]=='C'){
+                            result+=90;
+                            i++;
+                        }
+                        else{
+                            result+=10;
+                            
+                        }
+                    break;
+                case 'L':
+                        result+=50;
+                    break;
+                case 'C':
+                        if(s[i+1]=='D'){
+                            result+=400;
+                            i++;
+                        }
+                        else if(s[i+1]=='M'){
+                        result+=900;
+                        i++;
+                        }
+                        else{
+                        result+=100;
+                        
+                        }
+                    break;
+                case 'D':
+                        result+=500;
+                    break;
+                case 'M':
+                        result+=1000;
+            }
+        }
+       return result; 
+        
+    }
+};
