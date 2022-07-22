a5b273267039ea8ae2ae58d944ee06958f95711f
yashwanth008 <76914360+yashwanth008@users.noreply.github.com>
2022-07-22 23:31:33 +0530

    Create NGE_arr.cpp

---
+++ NGE_arr.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        vector<long long> ans = arr;
+        for (int i = 0; i < n; i++){
+        ans[i] = -1;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                ans[i] = arr[j];
+                break;
+            }
+        }
+            
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
